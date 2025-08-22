300d70d3b6117ae2c4a39c2c330c02dc4c449213
Alexandr <thepro1559@gmail.com>
2025-08-21 20:57:02 -0600

    auth wip

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {AuthController} from './auth.controller';
+import {AuthService} from './auth.service';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+    imports: [ConfigModule.forRoot({isGlobal: true})],
+    controllers: [AuthController],
+    providers: [AuthService],
+    exports: [AuthService],
 })
 export class AuthModule {}
--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {randomBytes, createHash} from 'crypto';
+
+function base64urlEncode(buffer: Buffer): string {
+    return buffer.toString('base64')
+        .replace(/=/g, '') // Remove padding
+        .replace(/\+/g, '-') // Replace '+' with '-'
+        .replace(/\//g, '_');
+}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    generatePkce() {
+        const codeVerifier = base64urlEncode(randomBytes(32));
+        const codeChallenge = base64urlEncode(
+            createHash('sha256').update(codeVerifier).digest()
+        );
+        return {codeVerifier, codeChallenge};
+    }
+}
