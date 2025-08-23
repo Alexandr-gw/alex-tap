842ef92a5a208e0260d33ee35176fd6670338545
Alexandr <thepro1559@gmail.com>
2025-08-23 09:57:01 -0600

    login controller added

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Req, Res, Get, Post} from '@nestjs/common';
+import {AuthService} from './auth.service';
+import {ConfigService} from "@nestjs/config";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly AuthService: AuthService, private cfg: ConfigService) {
+    }
+
+    @Get('login')
+    login(@Req() req, @Res() res) {
+        const {codeVerifier, codeChallenge} = this.AuthService.generatePkce();
+        const state = crypto.randomUUID();
+        const nonce = crypto.randomUUID();
+
+        res.cookie('pkce_verifier', codeVerifier, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'lax',
+            maxAge: 5 * 60 * 1000
+        });
+        res.cookie('oidc_state', state, {httpOnly: true, secure: true, sameSite: 'lax', maxAge: 5 * 60 * 1000});
+        res.cookie('oidc_nonce', nonce, {httpOnly: true, secure: true, sameSite: 'lax', maxAge: 5 * 60 * 1000});
+
+        const authorizationUrl = this.AuthService.buildAuthUrl({
+            authorizationEndpoint: this.cfg.getOrThrow<string>('OIDC_AUTHORIZATION_ENDPOINT'),
+            clientId: this.cfg.getOrThrow<string>('OIDC_CLIENT_ID'),
+            redirectUri: this.cfg.getOrThrow<string>('OIDC_REDIRECT_URI'),
+            challenge: codeChallenge,
+            state,
+            nonce,
+        });
+        return res.redirect(authorizationUrl);
+    }
+
+@Get('callback')
+
+@Post('refresh')
+
+@Post('logout')
+}
